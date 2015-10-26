37943c238e4321dd6f729f730dfcf5c6723ffb55
pratamawijaya <set.mnemonix@gmail.com>
2015-10-26 19:34:25 +0700

    init all

---
+++ app/src/main/java/com/pratamawijaya/firebasechat/base/BaseApplication.java
@@ -0,0 +1,14 @@
+package com.pratamawijaya.firebasechat.base;
+
+import android.app.Application;
+
+/**
+ * Created by : pratama - set.mnemonix@gmail.com
+ * Date : 10/26/15
+ * Project : FirebaseChat
+ */
+public class BaseApplication extends Application {
+  @Override public void onCreate() {
+    super.onCreate();
+  }
+}
